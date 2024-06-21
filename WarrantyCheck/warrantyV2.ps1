4199cc6f4def9e778e1cedb32eba4e0b27b173ee
oisnmcl1 <omclaughlin99@gmail.com>
2024-06-21 17:30:42 +0100

    Start of V2 script using different module

--- WarrantyCheck/warrantyV2.ps1
+++ WarrantyCheck/warrantyV2.ps1
@@ -1,1 +1,14 @@
-Write-Host "Hello World!"
+# Import the module
+Import-Module PSWarranty
+
+# Define your device's serial number and product number
+
+$deviceSerial = "5CG021392W"
+
+#$deviceSerial = "5CD3476QKC"
+
+# Get the warranty information
+$warrantyInfo = Get-WarrantyInfo -DeviceSerial $deviceSerial
+
+# Output the warranty information
+Write-Output $warrantyInfo
