98affdcd3d165639ee5a6ae6ab399d44e8777a98
Oisín McLaughlin <oisin@trojan-technologies.com>
2024-06-25 17:27:00 +0100

    Get firewall API key and export config (untested)

--- FirewallConfigExp/getConfig.ps1
+++ FirewallConfigExp/getConfig.ps1
@@ -1,10 +1,14 @@
 function Get-APIKey {
     param (
         [string]$firewallIp,
-        [securestring]$password
+        [securestring]$password = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
     )
-
     $username = "admin"
+
+    $uri = "https://$firewallIp/api/?type=keygen&user=$username&password=$password"
+    $response = Invoke-RestMethod -Uri $uri -Method Post
+
+    return $response.response.result.key
 }
 
 function Export-FirewallConfig {
@@ -13,8 +17,17 @@ function Export-FirewallConfig {
         [string]$apiKey,
         [string]$outputFile
     )
+
+    $uri = "https://$firewallIp/api/?type=export&category=configuration&key=$apiKey"
+    Invoke-WebRequest -Uri $uri -OutFile $outputFile
 }
 
-$firewallIp = Read-Host "Enter the IP address of the firewall"
-$password = Read-Host "Enter your password" -AsSecureString
-$outputFile = "C:"
+$firewallIp = Read-Host "IP of firewall"
+$password = Read-Host "Password" -AsSecureString
+$outputFile = "C:\PaloAltoConfig\config.xml"
+
+$apiKey = Get-APIKey -firewallIp $firewallIp -password $password
+Export-FirewallConfig -firewallIp $firewallIp -apiKey $apiKey -outputFile $outputFile
+
+# Hopefully gets API key using palo alto api?
+# Untested though!!!
