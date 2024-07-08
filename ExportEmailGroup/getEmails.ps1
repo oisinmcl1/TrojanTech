ba7728bf123a970bca52aff03b8e43a9aba8ac77
oisnmcl1 <omclaughlin99@gmail.com>
2024-07-08 17:31:18 +0100

    Check for module and retrieve ad group obj and members array

--- ExportEmailGroup/getEmails.ps1
+++ ExportEmailGroup/getEmails.ps1
@@ -1,1 +1,32 @@
-#Initial
+# Check if ad module is installed.
+if (-not (Get-Module -ListAvailable -Name ActiveDirectory)) {
+    Write-Host ("Active Directory Module not installed")
+    exit
+}
+
+# Prompt for name of AD
+$groupName = Read-Host -Prompt ("Enter AD group name")
+
+<#
+if (!$groupName) {
+    Write-Host ("No AD group entered")
+    exit
+}
+#>
+
+# Attempt to retrieve AD group obj based on entered group name
+try {
+    $group = Get-ADGroup -Identity $groupName -ErrorAction Stop
+}
+# Output message and exit if group doesn't exist
+catch {
+    Write-Host ("Group does not exist in AD")
+    exit
+}
+
+# Retrieve members array
+$members = Get-ADGroupMemeber -Identity $groupName
+
+
+# Array of emails retrieved
+$emailAddr = @()
--- ExportEmailGroup/readme.txt
+++ ExportEmailGroup/readme.txt
@@ -2,3 +2,6 @@ This script will get emails from a group in active directory.
 
 Given that it is run on a domain controller server,
 I'm assuming the active directory powershell module is already installed.
+
+Oisín Mc Laughlin
+08/07/2024
