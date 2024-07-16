e5634a184eed5b7e84cb3aa43c1321a39f20347e
oisinmcl1 <omclaughlin99@gmail.com>
2024-07-16 16:52:09 +0100

    Attempt to enable restful api through ssh

--- GetFortiVoiceAPI/getFortiVoiAPI.ps1
+++ GetFortiVoiceAPI/getFortiVoiAPI.ps1
@@ -1,1 +1,39 @@
-# init
+param(
+    [string]$ip,
+    [string]$username
+)
+
+
+
+# THIS SECTION ENABLES RESTFUL API THROUGH SSH
+
+# Check if SSH module is installed
+if(!(Get-Module -ListAvailable -Name Posh-SSH)) {
+    try {
+        Install-Module -Name Posh-SSH -Repository PSGallery -Force
+    }
+    catch {
+        Write-Error "Failed to install Posh-SSH $_"
+        exit
+    }
+}
+
+# Import SSH module
+try{
+    Import-Module Posh-SSH -ErrorAction Stop
+}
+catch {
+    Write-Error "Failed to Import Posh-SSH $_"
+    exit
+}
+
+# Establish Connection to FortiVoice System
+$sshSesh = New-SSHSession -ComputerName $ip -Credential (Get-Credential)
+
+# Run CLI commands to enable REST API
+Invoke-SSHCommand -SSHSession $sshSesh -Command "config system global"
+Invoke-SSHCommand -SSHSession $sshSesh -Command "set rest-api enable"
+Invoke-SSHCommand -SSHSession $sshSesh -Command "end"
+
+# Close SSH
+Remove-SSHSession -SSHSession $sshSesh
--- GetFortiVoiceAPI/readme.txt
+++ GetFortiVoiceAPI/readme.txt
@@ -1,1 +1,2 @@
-init
+https://github.com/darkoperator/Posh-SSH
+https://docs.fortinet.com/document/fortivoice-enterprise/6.4.7/fortivoice-phone-system-rest-api-reference-guide/225180/enabling-rest-api-support
