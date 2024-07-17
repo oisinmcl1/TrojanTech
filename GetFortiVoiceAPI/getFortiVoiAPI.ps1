1ce0fdf1371debba8f01f838b25b32a22a4bed19
oisinmcl1 <omclaughlin99@gmail.com>
2024-07-17 15:43:03 +0100

    Function to enable rest api and also installs NuGet if needed

--- GetFortiVoiceAPI/getFortiVoiAPI.ps1
+++ GetFortiVoiceAPI/getFortiVoiAPI.ps1
@@ -1,7 +1,7 @@
 param(
     [string]$ip,
-    [string]$username
-    ## [string]$password
+    [string]$username,
+    [string]$password
 )
 
 # Function to check if module is installed and import it
@@ -10,8 +10,15 @@ function ModuleInstall {
         [string]$moduleName
     )
 
-    # Update NuGet to install needed modules
-    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+    # NuGet needed for these modules, check if installed, if not install
+    if (!(Get-PackageProvider -Name NuGet)) {
+        try {
+            Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+        }
+        catch {
+            Write-Error "Failed to install NuGet: `n $_"
+        }
+    }
 
     # Install module if not installed already
     if(!(Get-Module -ListAvailable -Name $moduleName)) {
@@ -19,7 +26,7 @@ function ModuleInstall {
             Install-Module -Name $moduleName -Repository PSGallery -Force
         }
         catch {
-            Write-Error "Failed to install $moduleName :`n $_"
+            Write-Error "Failed to Install $moduleName :`n $_"
             exit
         }
     }
@@ -36,20 +43,49 @@ function ModuleInstall {
 
 ## ModuleInstall -moduleName "Posh-SSH"
 ModuleInstall -moduleName "PSWriteHTML"
+ModuleInstall -moduleName "Invoke-RestMethod"
 
-<#
 
-# THIS SECTION ENABLES RESTFUL API THROUGH SSH
+# Function to enable rest API through SSH
+function Enable-RestAPI {
+    param(
+    [string]$ip,
+    [string]$username,
+    [string]$password
+)
 
-# Establish Connection to FortiVoice System
-$sshSesh = New-SSHSession -ComputerName $ip -Credential (Get-Credential)
+    # Establish Connection to FortiVoice System
+    $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, (ConvertTo-SecureString -String $password -AsPlainText -Force)
+    $sshSesh = New-SSHSession -ComputerName $ip -Credential $credential
 
-# Run CLI commands to enable REST API
-Invoke-SSHCommand -SSHSession $sshSesh -Command "config system global"
-Invoke-SSHCommand -SSHSession $sshSesh -Command "set rest-api enable"
-Invoke-SSHCommand -SSHSession $sshSesh -Command "end"
+    # Run CLI commands to enable REST API
+    Invoke-SSHCommand -SSHSession $sshSesh -Command "config system global"
+    Invoke-SSHCommand -SSHSession $sshSesh -Command "set rest-api enable"
+    Invoke-SSHCommand -SSHSession $sshSesh -Command "end"
 
-# Close SSH
-Remove-SSHSession -SSHSession $sshSesh
+    # Close SSH
+    Remove-SSHSession -SSHSession $sshSesh
+}
 
-#>
+# THIS SECTION AUTHENTICATES FOR FORTIVOICE API AND RETRIEVES APSSCOOKIE
+
+function Get-FortiCookie {
+    param(
+    [string]$ip,
+    [string]$username,
+    [string]$password
+    )
+
+    $url = "https://$ip/api/v1/VoiceAdminLogin"
+}
+
+
+
+# Enable Rest API
+try {
+    Write-Output "Enabling Rest-API`n"
+    Enable-RestAPI -ip $ip -username $username -password $password
+}
+catch {
+    Write-Error "Failed to enable Rest API: `n $_"
+}
