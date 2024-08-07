ad7b671d4377c7eeb0a18f7ae16c3d509dab6e22
oisinmcl1 <omclaughlin99@gmail.com>
2024-08-07 13:39:00 +0100

    Prompts for ip and interval if no parameters, added comments

--- PingMonitor/pingMonitor.ps1
+++ PingMonitor/pingMonitor.ps1
@@ -3,21 +3,35 @@ param(
     [string]$interval
 )
 
+# If no ip or interval provided as parameters, prompt using read-host
+if (-not $ip) {
+    $ip = Read-Host "Ip of Device"
+}
+if (-not $interval) {
+    $interval = Read-Host "Interval of ping in seconds"
+}
+
+# Date for naming of log file
 $dateID = Get-Date -Format yyyyMMdd-HHmmss
 $logfile = "pinglog-$dateID.txt"
+#Location for cli output
 $dir = Get-Location
 
-Write-Output "=======================================`nStarting Ping Monitor at $dateID for:`n$ip`n`nLog file location:`n$dir\$logfile`n`nInterval:`n$interval seconds`n`nPress Control-C to stop.`n======================================="
+Write-Output "=================================================`nStarting Ping Monitor at $dateID for:`n$ip`n`nLog file location:`n$dir\$logfile`n`nInterval:`n$interval seconds`n`nPress Control-C to stop.`n================================================="
 while ($true) {
+    # Get timestamp and ping device
     $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
     $ping = Test-Connection -ComputerName $ip -Count 1 -ErrorAction SilentlyContinue
     
+    # Log status if ping is online or offline
     if ($ping) {
         $status = "Online"
-    } else {
+    } 
+    else {
         $status = "Offline"
     }
 
+    # Append log to txt file and sleep for duration of interval
     "$timestamp - $status" | Out-File -FilePath $logfile -Append
     Start-Sleep -Seconds $interval
 }
--- PingMonitor/readme.txt
+++ PingMonitor/readme.txt
@@ -3,11 +3,14 @@ Script just quickly made to ping an ip of your desire and log the response and p
 https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/test-connection?view=powershell-7.4
 
 
-Usage:
-
 .\pingMonitor -ip "Ip address of device to monitor" -interval "Interval of pings in seconds"
 
+If no parameter's provided for ip or interval, you will be prompted in cli.
+
 Press Control-C to stop monitor
+
+
+Output file will go into same directory as script, output location is also outputted in cli.
 
 Oisín Mc Laughlin
 07/08/2024
