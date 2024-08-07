bb1855415d7bd6c222ecf2bfd7b8564b8502eec0
oisinmcl1 <omclaughlin99@gmail.com>
2024-08-07 12:24:01 +0100

    Now log files save in the format of pingLog-date

--- PingMonitor/pingMonitor.ps1
+++ PingMonitor/pingMonitor.ps1
@@ -3,10 +3,11 @@ param(
     [string]$interval
 )
 
-$logfile = "pinglog.txt"
+$dateID = Get-Date -Format yyyyMMdd-HHmmss
+$logfile = "pinglog-$dateID.txt"
 $dir = Get-Location
 
-Write-Output "=======================================`nStarting Ping Monitor for:`n$ip`n`nLog file location:`n$dir`n`nInterval:`n$interval seconds`n`nPress Control-C to stop.`n======================================="
+Write-Output "=======================================`nStarting Ping Monitor at $dateID for:`n$ip`n`nLog file location:`n$dir\$logfile`n`nInterval:`n$interval seconds`n`nPress Control-C to stop.`n======================================="
 while ($true) {
     $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
     $ping = Test-Connection -ComputerName $ip -Count 1 -ErrorAction SilentlyContinue
