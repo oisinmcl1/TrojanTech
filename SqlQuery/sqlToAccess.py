d6221c85a1eb530042e4456f13a5ead788667435
oisinmcl1 <omclaughlin99@gmail.com>
2024-08-15 13:06:54 +0100

    Update the access database

--- SqlQuery/sqlToAccess.py
+++ SqlQuery/sqlToAccess.py
@@ -59,7 +59,7 @@ try:
 # If error, print message and ensure rows is none
 except pyodbc.Error as ex:
     print(f"SQL Server connection or query failed: {ex}")
-    rows = None
+    row = None
 
 
 # Close connection
@@ -77,7 +77,7 @@ CONNECT TO ACCESS DATABASE
 UPDATE HEADING, HEADING2 AND HEADINGEVONET
 """
 
-mdb = r"E:\BM\FuturaFrames\Orders\2024\Jun\RB262.mdb"
+mdb = r"E:\BM\FuturaFrames\Orders\2024\Jun\Test\RB262.mdb"
 
 try:
     # Connection string to Access database
@@ -92,6 +92,49 @@ try:
     accessCursor = accessConn.cursor()
 
     print(f"Connection to Access Database: {mdb} was successful!")
+
+    if row:
+        # Update Heading table in Access where Reference = 'RB2362'
+        updateQuery_Heading = """
+        UPDATE Heading
+        SET AccountNo = ?, AcknowledgementStatus = ?, AmountPaid = ?,
+            AmountPaid_Converted = ?, Architect = ?, Archive = ?, ArchivedCopyKeyID = ?, ArchiveZip = ?,
+            BatchIndexOrder = ?, BatchKeyID = ?, BMGlassJob = ?, BrickCavity = ?, BuyUnglazedFrameGlass = ?,
+            BuyUnglazedFromSupplier = ?, BuyPanelsFrom3rdParty = ?, ChangeDate = ?, ChangeTime = ?, CheckedOutTo = ?,
+            CheckOrderCreated = ?, Completed = ?, Contact = ?, Cost = ?, CostLibrary = ?, CounterUniqueKeyID = ?,
+            CreatedFrom = ?, CustomerID = ?, CustomerType = ?, CustomerUseMarkupPerColour = ?,
+            CustomerUsePrimeAperture = ?, CustomerMidrailHeight = ?, CustomerTransomDrop = ?, DateAmended = ?,
+            DateCompleted = ?, DateConfirmed = ?, DateCreated = ?, DateDelivery = ?, DateFitted = ?, DateGlassDelivery = ?,
+            DateInProduction = ?, DateOnwardDelivery = ?, DateInvoice = ?, DateLoaded = ?, DateOrder = ?, DatePaid = ?,
+            DatePaidSeveral = ?, DatePanelDelivery = ?, DateRoofDelivery = ?, DateSawFileCreated = ?, DateSurvey = ?,
+            DateToughGlassDelivery = ?, DatePreOrderConverted = ?, DatePreQuoteConverted = ?, Despatched = ?,
+            Dessian_CurrencyConversion = ?, Email = ?, EmailLong = ?, FaxNo = ?, FensaNumber = ?, FileName = ?, Fitting = ?,
+            FittingAdvancedID = ?, FittingTeam = ?, FittingTeamID = ?, GlassAccountNo = ?, GlassBatched = ?,
+            GlassSupplierID = ?, Glazed = ?, Hidden = ?, HousetypeCategoryID = ?, Initials = ?, InputBy = ?, InputByID = ?,
+            InvoiceAddress1 = ?, InvoiceAddress2 = ?, InvoiceAddress3 = ?, InvoiceAddress4 = ?, InvoiceCounty = ?,
+            InvoiceNumber = ?, InvoicePostCode = ?, InvoicePrinted = ?, InvoiceSettled = ?, InvoiceSettledInformation = ?,
+            JobKeyID = ?, JobNumber = ?, JobType = ?, MainColour = ?, MasterJobKeyID = ?, Mobile = ?, Name = ?,
+            NettDeliveryCharge = ?, NettDeliveryCharge_Converted = ?, Notes = ?, PanelSupplierID = ?, PaymentMethod = ?,
+            PaymentMethodID = ?, PhoneNo = ?, Posted = ?, PreOrderPrinted = ?, PreQuotePrinted = ?, PriceGrids = ?,
+            Projectname = ?, QuantityFabricationUnits = ?, QuantityFabricationUnits_Fabrication = ?, QuantityFrames = ?,
+            QuantityGlass = ?, QuantityGlassAnnealed = ?, QuantityGlassLaminated = ?, QuantityGlassObscure = ?,
+            QuantityGlassToughened = ?, QuantityPanels = ?, QuantityPanelsFlat = ?, QuantityPanelsMoulded = ?,
+            QuantityRoofs = ?, QuantityRoofPacks = ?, QuantityUnglazed = ?, QuoteNumber = ?, QuotePrinted = ?,
+            ReceivedOrder = ?, Reference = ?, RouteKeyID = ?, Salesman = ?, SalesmanCommission = ?, SalesmanID = ?,
+            Salutation = ?, SatelliteCustomerID = ?, SatelliteJobKeyID = ?, SatelliteJobNumber = ?, SatelliteName = ?,
+            SatelliteUploadedDate = ?, SellingPriceExTax = ?, SellingPriceExTax_Converted = ?, SellingPriceIncTax = ?,
+            SellingPriceIncTax_Converted = ?, TaxAmount = ?, TaxAmount_Converted = ?, TaxCode = ?, TaxRate = ?,
+            Toughened = ?, VersionCreated = ?, VersionAmended = ?
+        WHERE JobNumber = 'RB262';
+        """
+
+        # Execute the update query with the data fetched from SQL Server
+        accessCursor.execute(updateQuery_Heading, *row)
+
+        # Commit the transaction
+        accessConn.commit()
+
+        print("Access database updated successfully.")
 
 # If error, print error message
 except pyodbc.Error as ex:
