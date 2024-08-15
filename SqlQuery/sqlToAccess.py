8d90705f4c45a0d64e6144df8bdffb080807a542
oisinmcl1 <omclaughlin99@gmail.com>
2024-08-15 11:18:18 +0100

    Now connects to access database

--- SqlQuery/sqlToAccess.py
+++ SqlQuery/sqlToAccess.py
@@ -1,5 +1,11 @@
 import pyodbc
 
+
+"""
+CONNECT TO SQL SERVER
+FETCH DBO_HEADING, DBO_HEADING2 AND DBO_HEADINGEVONET
+"""
+
 dsn = "Orders"
 database = "FFOrders"
 
@@ -11,16 +17,58 @@ try:
 
     print(f"Connection to SQL Server: {dsn} and Database: {database} was successful!")
 
-# If error from pyodbc, print the SQLSTATE code
-except pyodbc.Error as ex:
-    sqlstate = ex.args[1]
-    print(f"An error occurred while trying to connect: {sqlstate}")
+    # Fetch Query from SQL Server
+    fetchQuery = ""
 
-# Try close connection if it was established
+    # Execute fetch query
+    ## sqlCursor.execute(fetchQuery)
+    ## rows = sqlCursor.fetchall()
+
+# If error, print message and ensure rows is none
+except pyodbc.Error as ex:
+    print(f"SQL Server connection or query failed: {ex}")
+    rows = None
+
+# Close connection
+finally:
+    sqlCursor.close()
+    sqlServer.close()
+    print("SQL Connection Closed.")
+
+
+
+
+
+"""
+CONNECT TO ACCESS DATABASE
+UPDATE HEADING, HEADING2 AND HEADINGEVONET
+"""
+
+mdb = r"E:\BM\FuturaFrames\Orders\2024\Jun\RB262.mdb"
+
+try:
+    # Connection string to Access database
+    connStr = (
+        r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};"
+        f"DBQ={mdb};"
+    )
+
+    # Try to connect to Access database and create cursor
+    accessConn = pyodbc.connect(connStr)
+
+    accessCursor = accessConn.cursor()
+
+    print(f"Connection to Access Database: {mdb} was successful!")
+
+# If error, print error message
+except pyodbc.Error as ex:
+    print(f"Access Database connection failed: {ex}")
+
+# Close connection
 finally:
     try:
-        sqlCursor.close()
-        sqlServer.close()
-        print("Connection closed.")
+        accessCursor.close()
+        accessConn.close()
+        print("Access Connection Closed.")
     except:
-        pass
+        print("No connection to close or error closing connection")
