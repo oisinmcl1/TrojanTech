9a3bdc2c66274ebf11d966c3324363faba342402
oisinmcl1 <omclaughlin99@gmail.com>
2024-08-23 15:23:48 +0100

    Now tcp connects and executes smtp commands (working)

--- TelnetEmailDebug/testEmail.ps1
+++ TelnetEmailDebug/testEmail.ps1
@@ -17,3 +17,31 @@ if (-not $toEmail) {
 }
 
 # Create a TCP connection to SMTP server on port 25
+$tcpClient = New-Object System.Net.Sockets.TcpClient($smtpServer, 25)
+$stream = $tcpClient.GetStream()
+$reader = New-Object System.IO.StreamReader($stream)
+$writer = New-Object System.IO.StreamWriter($stream)
+$writer.AutoFlush = $true
+
+# Read server response
+$reader.ReadLine()
+
+# Send Helo
+$writer.WriteLine("HELO")
+$reader.ReadLine()
+
+# Mail From
+$writer.WriteLine("mail from:<$fromEmail>")
+$reader.ReadLine()
+
+# Rcpt To
+$writer.WriteLine("rcpt to:<$toEmail>")
+$reader.ReadLine()
+
+# Close Connection
+$reader.Close()
+$writer.Close()
+$stream.Close()
+$tcpClient.Close()
+
+Write-Host "All SMTP commands sent successfully!"
